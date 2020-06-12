508e3ff1a9850fb06bd23c8bbb149a797612f11c
pporschen <accounts@pp83.de>
2020-06-12 12:30:50 +0200

    added queries

--- database/client.js
+++ database/client.js
@@ -5,6 +5,4 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log("connected"));
 
-console.log(db.tweets.find())
-
 module.exports = db;
