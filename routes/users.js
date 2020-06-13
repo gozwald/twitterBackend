d320cf6a71708d6cc5052db276931c68a0f8b4db
Samuel Greenwald <gozwald@gmail.com>
2020-06-13 17:19:46 +0200

    no really. Final

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,33 @@
 var express = require("express");
 var router = express.Router();
-const jwt = require('jsonwebtoken');
-const User = require('../database/models/User');
-const Tweets = require('../database/models/Tweets');
+const jwt = require("jsonwebtoken");
+const User = require("../database/models/User");
+const Tweets = require("../database/models/Tweets");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  User.find().then(data => res.json(data)).catch(err => console.log(err))
-});
-
-router.post("/", function (req, res, next) {
-  res.send("respond with a resource");
+  User.find()
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
 router.get("/:id/", function (req, res, next) {
-
   const { id } = req.params;
-  User.find({ _id: id }).then(data => res.json(data)).catch(err => console.log(err));
+  User.find({ _id: id })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
 router.get("/:id/messages/", function (req, res, next) {
   const { id } = req.params;
-  Tweets.find({ user_id: id }).then(data => res.json(data)).catch(err => console.log(err));
+  Tweets.find({ user_id: id })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body)
+  console.log(req.body);
 
   const query = await User.find({ email });
   const user = await query;
@@ -35,12 +36,10 @@ router.post('/login', async (req, res) => {
     const payload = { id: user._id };
     const token = jwt.sign(payload, "hellomyfirend123");
 
-    return res.json({ token })
+    return res.json({ token });
   }
 
-  res.send("fail")
-
+  res.send("fail");
 });
-
 
 module.exports = router;
