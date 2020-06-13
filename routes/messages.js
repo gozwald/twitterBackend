95cfcd8d0372e9288e1bbdf18831abb1459c12e0
Samuel Greenwald <gozwald@gmail.com>
2020-06-13 17:19:06 +0200

    final touches

--- routes/messages.js
+++ routes/messages.js
@@ -1,19 +1,19 @@
 var express = require("express");
 var router = express.Router();
-const Tweets = require('../database/models/Tweets');
+const Tweets = require("../database/models/Tweets");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  Tweets.find().then(data => res.json(data)).catch(err => console.log(err))
+  Tweets.find()
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
-
-// router.get("/:id", function (req, res, next) {
-//   res.send("respond with a resource");
-// });
 
 router.get("/:id/", (req, res) => {
   const { id } = req.params;
-  Tweets.find({ _id: id }).then(data => res.json(data)).catch(err => console.log(err))
+  Tweets.find({ _id: id })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
