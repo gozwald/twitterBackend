00fe5ce7928e3b3586f280ac274f4fc5fbe268c8
pporschen <58578026+pporschen@users.noreply.github.com>
2020-06-30 21:01:47 +0200

    Update client.js

--- database/client.js
+++ database/client.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox-szora.mongodb.net/twitter?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect(, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
